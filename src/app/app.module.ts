e760f45fea646f41e21c90870a0e36fc1d0d53f1
Joe Cui <joe.cui@bpanz.com>
2020-10-05 09:41:57 +1000

    Exported CollectionsHome Component from Collections Module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,11 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ElementsModule } from './elements/elements.module';
+import { CollectionsModule } from './collections/collections.module';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ElementsModule],
+  imports: [BrowserModule, AppRoutingModule, ElementsModule, CollectionsModule],
   providers: [],
   bootstrap: [AppComponent],
 })
