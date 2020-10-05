09a02917a621535efb4531fde51b3f240380ba1a
Joe Cui <joe.cui@bpanz.com>
2020-10-06 09:11:45 +1000

    Added bio/companies/partners tabs with Child Component Routing

--- src/app/collections/collections.module.ts
+++ src/app/collections/collections.module.ts
@@ -5,9 +5,20 @@ import { SharedModule } from '../shared/shared.module';
 import { CollectionsRoutingModule } from './collections-routing.module';
 import { CollectionsHomeComponent } from './collections-home/collections-home.component';
 import { TableComponent } from './table/table.component';
+import { BiographyComponent } from './biography/biography.component';
+import { CompaniesComponent } from './companies/companies.component';
+import { PartnersComponent } from './partners/partners.component';
+import { TabsComponent } from './tabs/tabs.component';
 
 @NgModule({
-  declarations: [CollectionsHomeComponent, TableComponent],
+  declarations: [
+    CollectionsHomeComponent,
+    TableComponent,
+    BiographyComponent,
+    CompaniesComponent,
+    PartnersComponent,
+    TabsComponent,
+  ],
   imports: [CommonModule, CollectionsRoutingModule, SharedModule],
   exports: [],
 })
