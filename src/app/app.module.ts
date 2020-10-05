d64997c611eaf632862b61dde63a676e7d04b079
Joe Cui <joe.cui@bpanz.com>
2020-10-05 11:10:08 +1000

    Added Home route and Not-Found route

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,10 +5,17 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ElementsModule } from './elements/elements.module';
 import { CollectionsModule } from './collections/collections.module';
+import { HomeComponent } from './home/home.component';
+import { NotFoundComponent } from './not-found/not-found.component';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, ElementsModule, CollectionsModule],
+  declarations: [AppComponent, HomeComponent, NotFoundComponent],
+  imports: [
+    BrowserModule,
+    ElementsModule,
+    CollectionsModule,
+    AppRoutingModule, // must be the last to catch '' and '**' routes
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
