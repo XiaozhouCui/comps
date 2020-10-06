c0107688008471e75dd868e840ed8033c4e4ee8d
Joe Cui <joe.cui@bpanz.com>
2020-10-06 14:12:42 +1000

    Added views/ItemList component with images

--- src/app/views/views-home/views-home.component.ts
+++ src/app/views/views-home/views-home.component.ts
@@ -12,6 +12,19 @@ export class ViewsHomeComponent implements OnInit {
     { value: 50, label: 'Reviews' },
   ];
 
+  items = [
+    {
+      image: '/assets/images/couch.jpeg',
+      title: 'Couch',
+      description: 'This is a fantastic couch to sit on.',
+    },
+    {
+      image: '/assets/images/dresser.jpeg',
+      title: 'Dresser',
+      description: 'This is a great dresser to put stuff in.',
+    },
+  ];
+
   constructor() {}
 
   ngOnInit(): void {}
